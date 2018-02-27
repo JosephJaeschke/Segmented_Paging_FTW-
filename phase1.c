fb254b4e9d95d4b09eee111204cacedc87897094
JosephJaeschke <joeyj987@gmail.com>
2018-02-27 14:02:34 -0500

    skeleton for phase1

---
+++ phase1.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+char* mem[8000000];
+
+char* myallocate(size_t size,char* file,int line,int type)
+{
+	return NULL;
+}
+
+void mydeallocate(size_t size,char* file,int line,int type)
+{
+	return;
+}
+
+int main()
+{
+
+}
