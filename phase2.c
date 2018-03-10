92e705011bf93dddd642491429d5adf0bdfae1e9
JosephJaeschke <joeyj987@gmail.com>
2018-03-10 17:56:51 -0500

    reformating for easier progrssion into rest of phases

--- phase2.c
+++ phase2.c
@@ -5,14 +5,17 @@
 #include <signal.h>
 #include <malloc.h>
 #include "mem.h"
+#include "my_pthread_t.h"
 
 #define malloc(x) myallocate(x,__FILE__,__LINE__,0);
 #define free(x) mydeallocate(x,__FILE__,__LINE__,0);
 #define VER 987
 
 char* mem;
-int init=0;
+int meminit=0;
 struct sigaction sa;
+int id=1;
+
 
 static void handler(int signum,siginfo_t* si,void* unused)
 {
@@ -31,7 +34,7 @@ int abs(int a)
 char* myallocate(size_t size,char* file,int line,int type)
 {
 	size+=sizeof(memHeader);
-	if(init==0)
+	if(meminit==0)
 	{
 		mem=(char*)memalign(sysconf(_SC_PAGE_SIZE),8000000);
 		sa.sa_flags=SA_SIGINFO;
@@ -39,56 +42,101 @@ char* myallocate(size_t size,char* file,int line,int type)
 		sa.sa_sigaction=handler;
 		if(sigaction(SIGSEGV,&sa,NULL)==-1)
 		{
-			printf("Fatal error in signal handler setup\n");
+			printf("Fatel error in signal handler setup\n");
 			exit(EXIT_FAILURE);
 		}
 		memHeader h;
-		h.free=0;
-		h.next=mem+size;
+		h.free=1;
 		h.prev=NULL;
+		h.next=mem+size;
 		h.verify=VER;
+		h.id=type;//CHANGE THIS FOR THREAD ID
 		memcpy((void*)mem,(void*)&h,sizeof(memHeader));
 		memHeader rest;
 		rest.free=1;
-		rest.next=mem+sizeof(mem); 
 		rest.prev=mem;
-		rest.verify=VER;
-		memcpy((void*)mem+size,(void*)&rest,sizeof(memHeader));
-		init=1;
-		return mem+sizeof(memHeader);
+		rest.next=mem+_SC_PAGE_SIZE;
+		rest.id=type;//CHANGE THIS FOR THREAD ID
+		memcpy((void*)mem,(void*)&rest,sizeof(memHeader));
 	}
-	//use Best Fit to find free spot
-	int best=8000000;
-	memHeader* bestBlock=NULL;
-	memHeader* headers=(memHeader*)mem;
-	while((char*)headers!=mem+sizeof(mem)) 
+	if(type!=0)
 	{
-		if(headers->free!=0)
+		//non-system request for mem
+		//add start index for non-system mem (ie don't start looking at 0)
+		char* ptr=mem;
+		while(ptr!=mem+sizeof(mem))
 		{
-			int sz=((char*)headers->next)-((char*)headers);//big enough for req size+header
-			if(abs(best-size)>abs(sz-size))
+			if(((memHeader*)ptr)->id==type)
 			{
-				best=sz;
-				bestBlock=headers;
+				//found its page!!
+				int best=_SC_PAGE_SIZE;
+				char* temp=ptr;
+				memHeader* bestFit=NULL;
+				memHeader* curr=(memHeader*)ptr;
+				while((char*)curr!=temp+_SC_PAGE_SIZE)
+				{
+					curr=(memHeader*)ptr;
+					if(curr->free!=0)
+					{
+						int sz=((char*)curr->next)-((char*)curr);//big enough for req size+header
+						if(abs(best-size)>abs(sz-size)&&(sz-size)>=0)
+						{
+							best=sz;
+							bestFit=curr;
+						}
+					}
+					curr=(memHeader*)curr->next;
+				}
+				if(bestFit==NULL)
+				{
+					return NULL;
+				}
+				memHeader rest;
+				bestFit->free=0;
+				char* tempNext=bestFit->next;
+				bestFit->next=(char*)bestFit+size;
+				rest.free=1;
+				rest.prev=(char*)bestFit;
+				rest.next=tempNext;
+				rest.verify=VER;
+				rest.id=bestFit->id;
+				memcpy((void*)(((void*)bestFit)+size),(void*)&rest,sizeof(memHeader));
+				return (char*)(((void*)bestFit)+sizeof(memHeader));
+			}
+			else if(((memHeader*)ptr)->id==0)
+			{
+				//found free page!!
+				memHeader new;
+				new.free=0;
+				new.prev=NULL;
+				new.next=ptr+size;
+				new.verify=VER;
+				new.id=type;//CHANGE FOR THREAD ID
+				memcpy((void*)ptr,(void*)&new,sizeof(memHeader));
+				memHeader rest;
+				rest.free=1;
+				rest.prev=ptr;
+				rest.next=ptr+_SC_PAGE_SIZE;
+				rest.verify=VER;
+				rest.id=type;//CHANGE FOR THREAD ID
+				memcpy((void*)(ptr+size),(void*)&rest,sizeof(memHeader));
+				return ptr+sizeof(memHeader);
+			}
+			else
+			{
+				ptr+=_SC_PAGE_SIZE;
 			}
 		}
-		headers=(memHeader*)headers->next;
-	}
-	if(bestBlock==NULL)
-	{
+		//no free pages
 		return NULL;
 	}
-	memHeader rest;
-	bestBlock->free=0;
-	char* tempNext=bestBlock->next;
-	bestBlock->next=(char*)((void*)bestBlock+size);
-	rest.free=1;
-	rest.next=tempNext;
-	rest.prev=(char*)bestBlock->prev;
-	rest.verify=VER;
-	memcpy((void*)bestBlock->next,(void*)&rest,sizeof(memHeader));
-	//printf("bb ver=%d\n",bestBlock->verify);
-	return (char*)((void*)bestBlock+sizeof(memHeader));
+	else
+	{
+		//system wants mem
+		//os mem should go in front of mem array
+		//~25-50% of array and should never go in swap
+	}
+	return NULL;
 }
 
 void coalesce(char* ptr)
@@ -145,6 +193,11 @@ void mydeallocate(char* ptr,char* file,int line,int type)
 	if(((memHeader*)((void*)ptr-sizeof(memHeader)))->verify!=VER)
 	{
 		printf("ERROR: Not pointing to void addr\n");
+		return;
+	}
+	if(((memHeader*)((void*)ptr-sizeof(memHeader)))->id!=type)//change for TID
+	{
+		printf("ERROR: You do not own this memory\n");
 		return;
 	}
 	((memHeader*)((void*)ptr-sizeof(memHeader)))->free=1;
