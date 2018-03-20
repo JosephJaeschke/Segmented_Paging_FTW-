81cd6dd6da990f22e1bc01c887e26204c6615034
JosephJaeschke <joeyj987@gmail.com>
2018-03-20 02:48:14 -0400

    current working copies as of multi

--- makefile
+++ makefile
@@ -2,6 +2,10 @@ all:
 	gcc -g -o phase2 phase2.c
 malloc:
 	gcc -Wall -g -o malloc mymalloc.c
+v:
+	gcc -g -o virtual virtual.c
+m:
+	gcc -g -o multi multi.c
 clean:
 	rm malloc
 	rm phase2
--- mem.h
+++ mem.h
@@ -4,43 +4,22 @@
 
 typedef struct memHeader_
 {
-	short segNum; //ASSUMPTION: all memHeaders that start at a page have a valid segNum. in fact, ALL memHeaders will have their segNum
+	short segNum; //don't think this is needed
 	short verify;
 	char* next;
 	char* prev;
 	short free:1;
-	int id;
-	/*
-	struct pageInfo_
-	{
-		short has_info:1; //info as to whether a memHeader holds info for the whole page, will always be set
-		int id;
-		short first_in_chain:1; //whether this is the first page in the list of pages thread is responsible for
-		struct memHeader_* next_page; //list of pages this thread is responsible for
-	} page_info;
-	*/
+	int id;	//this is not needed
 } memHeader;
 
 typedef struct memBook_
 {
 	int tid;
-	int pageNum;
+	int pageNum;//first,second,... page of the thread so we know where to put it
 	int used:1;
-	short first_in_chain:1; //whether this is the first page in the list of pages thread is responsible for
+	short first_in_chain; //whether this is the first page in the list of pages thread is responsible for
+	short numPages; //number of pages after that are part of 
 	char* next_page; //list of pages this thread is responsible for
-	size_t mem_left; //accessed from memHeader via segments[i].mem_left. _SC_PAGE_SIZE - sizeof(memHeader) is max size of a segment whose used == 1
 } memBook;
-
-
-/* adds threads name and segment number to a list of threads using dynamic alloation*/
-void add_thread(short segNum);
-
-/* deletes thread entry from threads using malloc, replaces tid with -2 (meaning used but empty)*/
-void remove_thread();
-
-/* returns next page that can fit request.  if none exist, returns NULL 
- * NOTE: does not check if page has large enough contiguous memory, just if it has enough*/
-memHeader* possible_page(memHeader* start, size_t target);
-
 
 #endif /* _MEM_H_ */
